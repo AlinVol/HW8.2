ed5bd68e71447ea6e3590a52860f3fa3753f5927
AlinVol <alina.voloshyna@gmail.com>
2024-06-04 22:43:04 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+Your task is to write a function is_palindrome that will check whether a string is a palindrome.
+
+A palindrome is a line that is read the same from left to right and from right to left without taking into account punctuation marks and the size of the letters.
+
+The function takes a string as input and returns a Boolean value of True or False
